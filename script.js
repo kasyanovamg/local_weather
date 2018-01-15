8a9b8925c9c0b62f16b1e3d6055db58dff2edd56
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-15 22:18:24 +0400

    feat: shows location

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  // Only change code below this line.
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
