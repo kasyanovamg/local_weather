575dd456315a4a126448e4053ed49168076e4710
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-15 23:07:53 +0400

    feat: shows weather

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-
   // Only change code below this line.
+  var lat, lon;
+function geolocator()  {
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
-    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  	lat = "lat=" + position.coords.latitude;
+  	lon = "lon=" + position.coords.longitude;
+    // $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+
+
+
+var api = "https://fcc-weather-api.glitch.me/api/current?";
+var urlString = api + lat + "&" + lon;
+
+
+$.getJSON(urlString, function(json) {
+	console.log(json);
+   		$("#city").html(json.name  + ", " + json.sys.country);
+   		$("#clouds").html(json.clouds.all);
+   		$("#temperature").html(json.main.temp);
+	});
+
+
+
   });
 }
+	
+}
+
+geolocator();
