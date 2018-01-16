a1b27010f25b6002c5cbd0aa334f4a964f43dc50
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-16 23:47:50 +0400

    feat: shows the weather and weather discription

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-  // Only change code below this line.
-  var lat, lon;
 function geolocator()  {
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
-  	lat = "lat=" + position.coords.latitude;
-  	lon = "lon=" + position.coords.longitude;
-    // $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 
+  	var lat = "lat=" + position.coords.latitude;
+  	var lon = "lon=" + position.coords.longitude;
 
-
-var api = "https://fcc-weather-api.glitch.me/api/current?";
-var urlString = api + lat + "&" + lon;
+	var api = "https://fcc-weather-api.glitch.me/api/current?";
+	var urlString = api + lat + "&" + lon;
 
 
 $.getJSON(urlString, function(json) {
 	console.log(json);
    		$("#city").html(json.name  + ", " + json.sys.country);
-   		$("#clouds").html(json.clouds.all);
+   		$("#status").html(json.weather["0"].description);
    		$("#temperature").html(json.main.temp);
+   		var icon = "<img src=" + json.weather["0"].icon + ">"; 
+   		$("#icon").html(icon);
+
+   		$("#c-f").on("click", function() {
+   			$(this).text($(this).text() == 'F' ? 'C' : 'F');
+  			});
 	});
 
 
 
   });
+	} else {
+		$("body").html("Geolocation is turned off in your browesr");
+	}
 }
 	
-}
+
 
 geolocator();
